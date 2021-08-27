55c75a9c07651bd532fc31cf037743bd68cd2954
SYadanar <suyadanar2001@gmail.com>
2021-08-27 16:59:45 +0630

    Changed App Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Material App Wahaha',
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
     );
